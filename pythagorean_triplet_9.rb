4230cd6824c11dc15e62325df87c84f96bd1c479
Fully34 <cfullinw34@gmail.com>
2015-04-20 09:11:28 -0600

    finished product_series_8, start pythagorean_triplet_9.rb

---
+++ pythagorean_triplet_9.rb
@@ -0,0 +1,17 @@
+=begin
+	
+A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+
+a2 + b2 = c2
+For example, 32 + 42 = 9 + 16 = 25 = 52.
+
+There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+Find the product abc.
+
+=end
+
+def pythag(a,b,c)
+	(a**2) + (b**2) = (c**2)
+end
+
+pythag(3,4,5)
